0e8c017341b6e44692df6fc51b4385a3f1ef8079
Yantasticc <yashkalamkhede2002@gmail.com>
2023-11-12 12:48:08 +0530

    layout

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -13,7 +13,7 @@ const Hero = () => {
     <div className='hero'>
       <div className='flex-1 pt-36 padding-x'>
         <h1 className='hero__title'>
-            Find, book, or rent a car - qickly and easily!
+            Find, book, or rent a car - quickly and easily!
         </h1>
         <p className='hero__subtitle'>Streamline your car rental experience with our effortless booking process.</p>
         <CustomButton 
@@ -29,8 +29,8 @@ const Hero = () => {
                 alt='hero'
                 fill className='object-contain'
             />
-            <div className='hero__image-overlay'></div>
         </div>
+        <div className='hero__image-overlay'></div>
       </div>
     </div>
   )
